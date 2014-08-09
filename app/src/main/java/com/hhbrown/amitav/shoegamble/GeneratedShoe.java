b6a7b70a23f4903ef3687a5bb22b1fa60de4318e
amitav13 <amitav.khandelwal@gmail.com>
2014-08-09 12:22:58 -0400

    Untested asynctask without oAuth

--- app/src/main/java/com/hhbrown/amitav/shoegamble/GeneratedShoe.java
+++ app/src/main/java/com/hhbrown/amitav/shoegamble/GeneratedShoe.java
@@ -3,13 +3,22 @@ package com.hhbrown.amitav.shoegamble;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class GeneratedShoe extends Activity {
@@ -62,5 +71,87 @@ public class GeneratedShoe extends Activity {
             gender = i.getStringExtra("gender");
             return rootView;
         }
+        public class FetchShoesTask extends AsyncTask<Void , Void, Void>{
+
+            private final String LOG_TAG = FetchShoesTask.class.getSimpleName();
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                HttpURLConnection urlConnection = null;
+                BufferedReader reader = null;
+                final String MEN_BASE_URL = "https://www.apitite.net/api/hhbrown/menminmax/json?";
+                final String WOMEN_BASE_URL = "https://www.apitite.net/api/hhbrown/menminmax/json?";
+
+                String shoesJsonStr = null;
+                try {
+                    Uri builtUri= null;
+                    if (gender.equals("Male")){
+                        builtUri = Uri.parse(MEN_BASE_URL).buildUpon()
+                                .appendQueryParameter("maxprice",max_price)
+                                .appendQueryParameter("minprice",min_price)
+                                .build();
+                    }else if (gender.equals("Female")){
+                        builtUri = Uri.parse(WOMEN_BASE_URL).buildUpon()
+                                .appendQueryParameter("maxprice",max_price)
+                                .appendQueryParameter("minprice",min_price)
+                                .build();
+                    }
+                    URL url= new URL(builtUri.toString());
+
+
+                    Log.v(LOG_TAG,"Built uri: "+builtUri.toString());
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.connect();
+
+                    // Read the input stream into a String
+                    InputStream inputStream = urlConnection.getInputStream();
+                    StringBuffer buffer = new StringBuffer();
+                    if (inputStream == null) {
+                        // Nothing to do.
+                        return null;
+                    }
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                        // But it does make debugging a *lot* easier if you print out the completed
+                        // buffer for debugging.
+                        buffer.append(line + "\n");
+                    }
+
+                    if (buffer.length() == 0) {
+                        // Stream was empty.  No point in parsing.
+                        return null;
+                    }
+                    shoesJsonStr = buffer.toString();
+                    Log.v(LOG_TAG, "Shoes JSON string: "+shoesJsonStr);
+
+
+
+                }catch (IOException e){
+                    Log.e(LOG_TAG,"Error", e);
+                }
+                finally
+                {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (final IOException e) {
+                            Log.e(LOG_TAG , "Error closing stream", e);
+                        }
+                    }
+                }
+
+
+            return null;
+            }
+        }
+
     }
 }
